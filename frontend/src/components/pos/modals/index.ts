4525e2628129f23138149bf5e14d7890655415e3
Yair Olvera <yairolvera778@gmail.com>
2026-06-17 22:35:56 -0600

    refactor: Fase 5C.1 - extracción de 4 modales simples del Dashboard

---
+++ frontend/src/components/pos/modals/index.ts
@@ -0,0 +1,4 @@
+export { default as PriceLookupModal } from "./PriceLookupModal";
+export { default as CancelSaleModal } from "./CancelSaleModal";
+export { default as CloseOptionsModal } from "./CloseOptionsModal";
+export { default as PartialCutSummaryModal } from "./PartialCutSummaryModal";
