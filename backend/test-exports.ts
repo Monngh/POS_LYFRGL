775bc3e21d12aa377086d0c5649b12df1b70815a
gaelo <gaelhernandezmonroy@gmail.com>
2026-06-26 09:26:47 -0600

    chore: mover tests a scratch y desvincular carpetas ignoradas

--- backend/test-exports.ts
+++
@@ -1,5 +0,0 @@
-import * as sc from "./src/controllers/sale.controller";
-console.log("Exported functions:");
-for (const key of Object.keys(sc)) {
-  console.log(`- ${key}: ${typeof (sc as any)[key]}`);
-}
